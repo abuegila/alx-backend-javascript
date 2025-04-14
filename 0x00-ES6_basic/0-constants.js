90b96f105245cdeeb69fc433a3a4000b9c05e7ac
abuegila <yusufabuegila@gmail.com>
2025-04-14 14:47:18 +0200

    first trial

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combo = `But sometimes let${getLast()}`;
+  return combo;
+}
